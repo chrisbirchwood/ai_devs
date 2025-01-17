41b6c0e5f1edcc276f5c3ab2cef31481b408f404
Chris Birchwood <krzysztof.brzezina@gmail.com>
2025-01-17 18:13:32 +0100

    Update map to match 4x4 grid image

---
+++ src/data/map.ts
@@ -0,0 +1,88 @@
+export const mapDescription = {
+  grid: [
+    // Pierwszy rząd (góra)
+    {
+      position: "1,1",
+      description: "start",
+      type: "start",
+    },
+    {
+      position: "1,2",
+      description: "trawa",
+      type: "grass",
+    },
+    {
+      position: "1,3",
+      description: "trawa, drzewo",
+      type: "tree",
+    },
+    {
+      position: "1,4",
+      description: "dom",
+      type: "house",
+    },
+    // Drugi rząd
+    {
+      position: "2,1",
+      description: "trawa",
+      type: "grass",
+    },
+    {
+      position: "2,2",
+      description: "wiatrak, trawa",
+      type: "windmill",
+    },
+    {
+      position: "2,3",
+      description: "trawa",
+      type: "grass",
+    },
+    {
+      position: "2,4",
+      description: "trawa",
+      type: "grass",
+    },
+    // Trzeci rząd
+    {
+      position: "3,1",
+      description: "trawa",
+      type: "grass",
+    },
+    {
+      position: "3,2",
+      description: "trawa",
+      type: "grass",
+    },
+    {
+      position: "3,3",
+      description: "kamienie",
+      type: "rocks",
+    },
+    {
+      position: "3,4",
+      description: "drzewa, trawa",
+      type: "trees",
+    },
+    // Czwarty rząd (dół)
+    {
+      position: "4,1",
+      description: "góry",
+      type: "mountains",
+    },
+    {
+      position: "4,2",
+      description: "góry",
+      type: "mountains",
+    },
+    {
+      position: "4,3",
+      description: "samochód",
+      type: "car",
+    },
+    {
+      position: "4,4",
+      description: "jaskinia",
+      type: "cave",
+    },
+  ],
+};
